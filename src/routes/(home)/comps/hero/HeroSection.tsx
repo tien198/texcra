3f704d0d5013fdf75c1becffe34d130843312986
tien <tienvn998@gmail.com>
2026-09-07 13:16:19 +0700

    add visual to Hero

--- src/routes/(home)/comps/hero/HeroSection.tsx
+++ src/routes/(home)/comps/hero/HeroSection.tsx
@@ -1,3 +1,4 @@
+import { VisualScene } from '#/components/visual-canvas/VisualScene'
 import { CapabilityRail } from './CapabilityRail'
 import { HeroNarrative } from './HeroNarrative'
 import styles from './hero.module.css'
@@ -5,14 +6,9 @@ import styles from './hero.module.css'
 export function HeroSection() {
   return (
     <section id="top" className={styles.hero} aria-labelledby="hero-heading">
-      <div className={styles.artwork} aria-hidden="true">
-        <img
-          src="/images/continuum-craft.png"
-          alt=""
-          fetchPriority="high"
-          width="1645"
-          height="1029"
-        />
+      <div aria-hidden="true">
+        <VisualScene/>
+    
       </div>
       <div className={styles.atmosphere} aria-hidden="true" />
       <div className={styles.content}>
