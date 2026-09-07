b3723fc2e633ce55b880f1169b6eea3a52659043
tien <tienvn998@gmail.com>
2026-09-07 22:35:47 +0700

    update

--- src/routes/(home)/comps/hero/HeroSection.tsx
+++ src/routes/(home)/comps/hero/HeroSection.tsx
@@ -18,7 +18,8 @@ export function HeroSection() {
       <VisualScene eventSource={eventSource} />
       <div
         className={clsx(
-          'absolute z-[-1] inset-0 top-[101px] md:top-0 pointer-events-none',
+          'absolute z-[-1] inset-0 top-[101px] pointer-events-none',
+          'md:top-0',
           // 'bg-[linear-gradient(to_bottom,#061828_0%,#061828f5_11%,#06182800_40%,#06182800_60%,#061828_83%,#061828_100%)]',
           // 'md:bg-[linear-gradient(to_top,#061828_0%,#06182800_19.33%),linear-gradient(to_left,#061828f5_0%,#06182825_22%,#061828e5_76%,#061828_100%)]'
         )}
@@ -26,7 +27,9 @@ export function HeroSection() {
       />
       <div
         className={clsx(
-          'relative z-[1] flex flex-col justify-between min-h-[663px] md:min-h-[800px] max-w-[1600px] mx-auto px-6 py-8 pb-10 md:px-[5%] md:pt-[112px] md:pb-[50px]',
+          'relative z-[1] flex flex-col justify-between min-h-[663px] md:min-h-[800px] max-w-[1600px] mx-auto ',
+          'px-6 py-8 pb-10 ',
+          'md:px-[5%] md:pt-[112px] md:pb-[50px]',
         )}
       >
         <HeroNarrative />
